54d998b85b2e8383c270227c651bc7e275e47f44
Shushant Priyadarshi <profshushant@gmail.com>
2026-06-17 08:04:42 +0530

    better-auth integrated

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,22 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 import helmet from "helmet";
+import { toNodeHandler } from "better-auth/node"
+import {auth} from "./utils/auth.js"
 
 const app = express();
 app.use(helmet())
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
+
+// Mount all better-auth routes — must be BEFORE express.json()
+app.all("/api/auth/*splat", toNodeHandler(auth))
+
 app.use(express.json());
 
 
